c9bf8a82f458e037662371d4b74c8585cedc2bcb
moduloscript <maduemeka254@gmail.com>
2023-03-12 00:16:21 +0100

    built the Homesearchinput component

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images:{
+    domains:["assets.stickpng.com"]
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import HomeHeader from "../components/HomeHeader";
+import HomeSearch from "../components/HomeSearch";
 
 export default function HomePage() {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image 
+          width="300"
+          height="100"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"
+        />
+        <HomeSearch />
+      </div>
+
     </>
   )
 }

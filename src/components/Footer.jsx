d65c3b17f56197640ea4b533e5b8b175f954d284
Moduloscript <maduemeka254@gmail.com>
2023-03-12 17:33:20 +0100

    first try

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,25 @@ import CountryLookup from "./CountryLookup";
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full">
-      <div className="border-b px-8 py-3 ">
+      <div className="px-8 py-3 border-b ">
         <CountryLookup />
       </div>
       <div
-        className="flex flex-col sm:flex-row  justify-between items-center px-8 py-3 space-y-7 sm:space-y-0"
+        className="flex flex-col items-center justify-between px-8 py-3 sm:flex-row space-y-7 sm:space-y-0"
       >
         <ul className="flex items-center space-x-6">
-          <li className="link cursor-pointer">Home</li>
-          <li className="link cursor-pointer">About</li>
-          <li className="link cursor-pointer">Advertisting</li>
-          <li className="link cursor-pointer">Business</li>
-          <li className="link cursor-pointer">How Search Works</li>
+          <li className="cursor-pointer link">Home</li>
+          <li className="cursor-pointer link">About</li>
+          <li className="cursor-pointer link">Advertisting</li>
+          <li className="cursor-pointer link">Business</li>
+          <li className="cursor-pointer link">How Search Works</li>
         </ul>
         <ul className="flex items-center space-x-6">
-          <li className="link cursor-pointer items-centerointer">
+          <li className="cursor-pointer link items-centerointer">
             Privacy
           </li>
-          <li className="link cursor-pointer">Terms</li>
-          <li className="link cursor-pointer">Settings</li>
+          <li className="cursor-pointer link">Terms now</li>
+          <li className="cursor-pointer link">Settings</li>
         </ul>
       </div>
     </footer>

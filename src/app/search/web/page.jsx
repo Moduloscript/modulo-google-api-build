b81c29c1e975ca2c4198c5c45111e44215a40b0c
Moduloscript <maduemeka254@gmail.com>
2023-03-14 17:57:48 +0100

    create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import WebSearchResults from "../../../components/WebSearchResults";
 
 export default async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_API_KEY}&q=${searchParams.searchTerm}`
   );
@@ -25,13 +27,7 @@ export default async function WebSearchPage({ searchParams }) {
 
   return (
     <>
-      {results &&
-        results.map((result) => (
-          <div key={result.kind}>
-            <h1>{result.link}</h1>
-            <h1>{result.title}</h1>
-          </div>
-        ))}
+      {results && <WebSearchResults results={ data } />}
     </>
   );
 }
--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -21,7 +21,7 @@ export default function HomeSearch() {
       .then((res) => res.json())
       .then((data) => data[0]);
     if (!response) return;
-    router.push(`/search/image?searchTerm=${response}`);
+    router.push(`/search/web?searchTerm=${response}`);
     setRandomSearchLoading(false);
   }
   return (

e03f9a36506ca9076f1cdaee8494ab1fb65cfeb9
Moduloscript <maduemeka254@gmail.com>
2023-03-16 20:16:34 +0100

    add pagination component

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import PaginationButtons from "./PaginationButtons";
 
 export default function ImageSearchResults({ results }) {
   return (
-    <div className=" pb-24 mt-4">
+    <div className=" sm:pb-24 mt-4 pb-40">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items?.map((result) => (
-          <div key={result.link} className="mb-8">
+          <div key={result.link} className="mb-8 group-hover:underline">
             <div className="group-hover:underline">
               <Link
                 href={result.image.contextLink}
@@ -19,11 +20,16 @@ export default function ImageSearchResults({ results }) {
                 </h2>
               </Link>
               <Link href={result.image.contextLink}>
-                <p className="group-hover:underline text-gray-500">{result.snippet}</p>
+                <p className="group-hover:underline text-gray-500">
+                  {result.snippet}
+                </p>
               </Link>
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+      <PaginationButtons />
       </div>
     </div>
   );
